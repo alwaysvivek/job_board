79e0b666a483d3320480e2fc396976fddccee6d3
Vivek Dagar <vivekdagar2025@gmail.com>
2025-12-01 11:36:31 +0000

    db fixes

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'; // <--- THIS IS THE KEY ADDITION
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
